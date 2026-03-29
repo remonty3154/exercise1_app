5015c7462dd7c29b827a1219de97d3607641d7b0
Buryatus <shelfynomogoev@gmail.com>
2026-03-30 01:39:59 +0800

    Методы подключены из библиотеки

--- src/main/java/ru/Kirill/app/Main.java
+++ src/main/java/ru/Kirill/app/Main.java
@@ -1,4 +1,19 @@
 package ru.Kirill.app;
 
+import ru.Kirill.utils.StringUtils; // Импорт из ВАШЕЙ библиотеки
+
 public class Main {
+    public static void main(String[] args) {
+        UserMessage msg = new UserMessage("  kirill_student  ", "hello world");
+
+        String cleanName = StringUtils.toCamelCase(msg.getUserName());
+        String reversedContent = StringUtils.reverse(msg.getMessageContent());
+
+        System.out.println("Оригинальное имя: " + msg.getUserName());
+        System.out.println("Обработанное имя (CamelCase): " + cleanName);
+        System.out.println("Перевернутое сообщение: " + reversedContent);
+
+        // Демонстрация работы toString() от Lombok
+        System.out.println("Объект целиком: " + msg);
+    }
 }
--- src/main/java/ru/Kirill/app/UserMessage.java
+++ src/main/java/ru/Kirill/app/UserMessage.java
@@ -1,4 +1,11 @@
 package ru.Kirill.app;
 
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
 public class UserMessage {
+    private String userName;
+    private String messageContent;
 }
